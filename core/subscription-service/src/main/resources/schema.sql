98177f5d7c0e0f194edd6dac96d0ebcf4d574317
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2025-09-14 15:12:48 +0300

    add create microservices

--- core/subscription-service/src/main/resources/schema.sql
+++ core/subscription-service/src/main/resources/schema.sql
@@ -3,12 +3,12 @@ CREATE TABLE IF NOT EXISTS subscribers
 (
     id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT,
-    subscriber BIGINT,
+    subscriber BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS black_list
 (
     id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     user_id BIGINT NOT NULL,
-    block_user BIGINT,
+    block_user BIGINT
 );
