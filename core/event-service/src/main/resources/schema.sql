1258a36545c652b20f7fc421a35d00d325a8fe72
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2025-09-27 01:05:05 +0300

    add recommendations

--- core/event-service/src/main/resources/schema.sql
+++ core/event-service/src/main/resources/schema.sql
@@ -1,4 +1,3 @@
-<<<<<<<< HEAD:core/event-service/src/main/resources/schema.sql
 CREATE TABLE IF NOT EXISTS categories
 (
     id   BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -35,42 +34,4 @@ CREATE TABLE IF NOT EXISTS events
     rating             FLOAT,
     CONSTRAINT events_category_id_fk FOREIGN KEY (category_id) REFERENCES categories (id),
     CONSTRAINT state_values CHECK (state IN ('PENDING', 'PUBLISHED', 'CANCELED'))
-);
-
-CREATE TABLE IF NOT EXISTS compilations_events
-(
-    compilation_id BIGINT NOT NULL,
-    events_id      BIGINT NOT NULL,
-    CONSTRAINT compilations_events_compilation_id_fk FOREIGN KEY (compilation_id) REFERENCES compilations (id),
-    CONSTRAINT compilations_events_event_id_fk FOREIGN KEY (events_id) REFERENCES events (id)
-========
-CREATE TABLE IF NOT EXISTS requests
-(
-    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    event_id     BIGINT                      NOT NULL,
-    created      TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    requester_id BIGINT                      NOT NULL,
-    status       VARCHAR(10)                 NOT NULL,
-    CONSTRAINT requests_event_id_fk FOREIGN KEY (event_id) REFERENCES events (id),
-    CONSTRAINT requests_requester_id_fk FOREIGN KEY (requester_id) REFERENCES users (id),
-    CONSTRAINT status_values CHECK (status IN ('PENDING', 'CONFIRMED', 'REJECTED', 'CANCELED'))
-);
-
-
-
-CREATE TABLE IF NOT EXISTS subscribers
-(
-    id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT,
-    subscriber BIGINT,
-    CONSTRAINT subscribers_user_id FOREIGN KEY (user_id) REFERENCES users (id)
-);
-
-CREATE TABLE IF NOT EXISTS black_list
-(
-    id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    block_user BIGINT,
-    CONSTRAINT black_list_user_id FOREIGN KEY (user_id) REFERENCES users (id)
->>>>>>>> 4608889 (add create microservices):core/main-service/src/main/resources/schema.sql
 );
