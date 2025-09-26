7c5c0ce136e0d876c87e660716b6ecb4d7700aec
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2025-09-27 01:01:04 +0300

    add recommendations

--- core/subscription-service/src/main/java/ru/practicum/subscription/mapper/SubscriptionMapper.java
+++ core/subscription-service/src/main/java/ru/practicum/subscription/mapper/SubscriptionMapper.java
@@ -8,11 +8,6 @@ import ru.practicum.mapper.UserMapperInteraction;
 import ru.practicum.dto.SubscriptionDto;
 import ru.practicum.subscription.model.BlackList;
 import ru.practicum.subscription.model.Subscriber;
-<<<<<<<< HEAD:core/subscription-service/src/main/java/ru/practicum/subscription/mapper/SubscriptionMapper.java
-========
-import ru.practicum.user.repository.UserMapper;
-import ru.practicum.user.userAdmin.UserAdminService;
->>>>>>>> 4608889 (add create microservices):core/main-service/src/main/java/ru/practicum/subscription/mapper/SubscriptionMapper.java
 
 import java.util.List;
 import java.util.stream.Collectors;
