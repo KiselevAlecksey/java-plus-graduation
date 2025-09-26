6916ed3c10fbceca6363b089a5350da720246253
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2025-09-27 00:56:25 +0300

    add recommendations

---
+++ core/interaction-api/src/main/java/ru/practicum/dto/converter/EventToEventShortResponseDtoConverter.java
@@ -0,0 +1,35 @@
+package ru.practicum.dto.converter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ru.practicum.mapper.CategoryMapperInteraction;
+import ru.practicum.dto.EventShortResponseDto;
+
+import ru.practicum.mapper.UserMapperInteraction;
+import ru.practicum.model.Event;
+
+@RequiredArgsConstructor
+@Component
+public class EventToEventShortResponseDtoConverter implements Converter<Event, EventShortResponseDto> {
+
+   private final UserMapperInteraction userMapper;
+   private final CategoryMapperInteraction categoryMapper;
+
+    @Override
+    public EventShortResponseDto convert(Event source) {
+        return EventShortResponseDto.builder()
+                .id(source.getId())
+                .paid(source.getPaid())
+                .title(source.getTitle())
+                .views(source.getViews())
+                .eventDate(source.getEventDate())
+                .annotation(source.getAnnotation())
+                .initiator(userMapper.toUserShortDto(source.getInitiator()))
+                .category(categoryMapper.toCategoryDto(source.getCategory()))
+                .views(source.getViews())
+                .build();
+    }
+
+
+}
---
+++ core/interaction-api/src/main/java/ru/practicum/dto/converter/NewEventDtoToEvent.java
@@ -0,0 +1,30 @@
+package ru.practicum.dto.converter;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import ru.practicum.dto.NewEventDto;
+import ru.practicum.model.Event;
+import ru.practicum.model.Location;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Component
+public class NewEventDtoToEvent implements Converter<NewEventDto, Event> {
+
+    @Override
+    public Event convert(NewEventDto source) {
+        Event event = new Event();
+        event.setAnnotation(source.annotation());
+        event.setCreatedOn(LocalDateTime.now());
+        event.setDescription(source.description());
+        event.setEventDate(source.eventDate());
+        event.setPaid(source.paid());
+        event.setParticipantLimit(source.participantLimit());
+        event.setRequestModeration(source.requestModeration());
+        event.setTitle(source.title());
+        event.setLocation(new Location(source.location().getLat(), source.location().getLon()));
+        return event;
+    }
+}
