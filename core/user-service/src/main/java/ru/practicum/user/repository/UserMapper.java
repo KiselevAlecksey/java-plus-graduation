d02641c6e22f04842c353fe8341b08f4c3b19f46
Alecksey Kiselev <Kiselev.Alecksey@yandex.ru>
2025-09-27 01:00:35 +0300

    add recommendations

--- core/user-service/src/main/java/ru/practicum/user/repository/UserMapper.java
+++ core/user-service/src/main/java/ru/practicum/user/repository/UserMapper.java
@@ -1,15 +1,9 @@
 package ru.practicum.user.repository;
 
 import org.mapstruct.Mapper;
-<<<<<<<< HEAD:core/user-service/src/main/java/ru/practicum/user/repository/UserMapper.java
-
 import org.springframework.stereotype.Component;
 import ru.practicum.dto.UserDto;
 import ru.practicum.dto.UserShortDto;
-========
-import ru.practicum.user.dto.UserDto;
-import ru.practicum.user.dto.UserShortDto;
->>>>>>>> 4608889 (add create microservices):core/main-service/src/main/java/ru/practicum/user/repository/UserMapper.java
 import ru.practicum.user.model.User;
 
 @Mapper(componentModel = "spring")
